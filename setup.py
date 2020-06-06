30712344cba2febfa4845587f3123ad3ddca9f1a
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-06 15:39:41 +0530

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DragSama", # Replace with your own username
-    version="0.0.1",
+    version="0.1",
     author="DragSama",
     description="Trace.moe python wrapper",
     long_description=long_description,
