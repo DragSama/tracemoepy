48d974e00f52cd6d05e4fecbe3490dc727f8172a
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-06 14:02:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DragSama", # Replace with your own username
+    version="0.0.1",
+    author="DragSama",
+    description="Trace.moe python wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DragSama/pytracemoe",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3),
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
