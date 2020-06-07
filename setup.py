670732fb544cea7ca6071346dfcbe35a46d7d229
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-07 22:43:22 +0530

    Change version to 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="tracemoepy",
     packagaes = ['tracemoepy'],
-    version="0.1",
+    version="0.2",
     description="Trace.moe python wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
