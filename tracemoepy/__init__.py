e04706f110aa956f008ae7e715a51e0dd40c7140
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-08 16:03:45 +0530

    Added docstring

--- tracemoepy/__init__.py
+++ tracemoepy/__init__.py
@@ -1,2 +1,11 @@
+"""Import all the classes."""
+
 from .tracemoe import TraceMoe
 from .errors import *
+
+__author__ = ["DragSama"]
+__version__ = 0.6
+
+if __name__ == '__main__':
+  from pprint import pprint
+  pprint(__author__, __version__, TraceMoe)
