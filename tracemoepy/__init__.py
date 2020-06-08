c297e211a0a5e9014fa61fa1b360fc125aec2fcb
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-08 21:29:44 +0530

    Import async_trace

--- tracemoepy/__init__.py
+++ tracemoepy/__init__.py
@@ -1,6 +1,7 @@
 """Import all the classes."""
 
 from .tracemoe import TraceMoe
+from .async_trace import Async_Trace
 from .errors import *
 
 __author__ = ["DragSama"]
@@ -8,4 +9,4 @@ __version__ = 0.6
 
 if __name__ == '__main__':
   from pprint import pprint
-  pprint(__author__, __version__, TraceMoe)
+  pprint(__author__, __version__, TraceMoe, Async_Trace)
