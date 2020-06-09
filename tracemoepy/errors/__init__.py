1b4f347a0ed1790dc7e20c163948e5c866b0d7f9
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-09 10:45:59 +0530

    Cleannnnnnnnnnn

--- tracemoepy/errors/__init__.py
+++ tracemoepy/errors/__init__.py
@@ -1,14 +1,21 @@
+"""All erorrs that are raised."""
+
 class TooManyRequests(Exception):
+
     """Raised when API Limit is reached or Too many requests in short period of time"""
 
 class EntityTooLarge(Exception):
+
     """Raised when image size > 10MB"""
 
 class ServerError(Exception):
+
     """Something wrong with the trace.moe server or Image provided was malformed"""
- 
+
 class InvalidToken(Exception):
+
     """Invalid token was provided"""
-    
+
 class EmptyImage(Exception):
+
     """Image provided was empty"""
